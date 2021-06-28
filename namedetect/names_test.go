1f0fc07823e67d670182985c3ebfa0dcf1d722a0
Anthony Ebiner <45410382+anthonyebiner@users.noreply.github.com>
2021-06-28 00:13:05 +0000

    Update names_test.go
    Add French names test

--- namedetect/names_test.go
+++ namedetect/names_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2019-2020 Segmed Inc.
+// Copyright (c) 2019-2021 Segmed Inc.
 // Author: Wojciech Adam Koszek <wkoszek@segmed.ai>
 package namedetect
 
@@ -11,10 +11,13 @@ import (
 func TestNames(t *testing.T) {
 	fnum := len(AllFirstNames)
 	lnum := len(AllLastNames)
+	rnum := len(AllFrenchNames)
 
 	fmt.Printf("number of first names: %d\n", fnum)
 	fmt.Printf("number of last names: %d\n", lnum)
+	fmt.Printf("number of french names: %d\n", rnum)
 
 	assert.Equal(t, fnum, 98400, "Number of first names doesn't match")
 	assert.Equal(t, lnum, 162255, "Number of last names doesn't match")
+	assert.Equal(t, lnum, 34262, "Number of french names doesn't match")
 }
